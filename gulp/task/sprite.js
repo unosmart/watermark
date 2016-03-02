a8fa2fa93120ea3d797f152c1484e60c67d9c01d
Maxim Ivanov <maxim_91@inbox.ru>
2016-03-02 21:37:54 +0300

    structure and builder fixed 2

--- gulp/task/sprite.js
+++ gulp/task/sprite.js
@@ -6,11 +6,13 @@ module.exports = function($) {
       .pipe($.sprite({
       imgName: 'sprite.png',
       cssName: '_sprite.scss',
+      imgPath:'../images/sprite.png',
       padding: 10,
       algorithm: 'top-down'
       }));
-      spriteData.img.pipe($.gulp.dest('./build/assets/css/'));
       spriteData.css.pipe($.gulp.dest('./source/style/'));
+      return spriteData.img.pipe($.gulp.dest('./build/assets/images/'));
   });
+
 
 };
