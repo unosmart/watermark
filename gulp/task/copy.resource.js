1b2532025d2a653d411930dc993c7878b1b8713e
Konstantin Kudelko <Konstantinkudelko>
2016-03-03 12:43:45 +0300

    Сетка

--- gulp/task/copy.resource.js
+++ gulp/task/copy.resource.js
@@ -2,7 +2,7 @@
 
 module.exports = function($) {
   $.gulp.task('copy:resource', function() {
-
+  
     return $.gulp.src('./source/images/*.*',{since:$.gulp.lastRun('copy:resource')})
       .pipe($.gulp.dest($.config.root+'/assets/images'))
   });
