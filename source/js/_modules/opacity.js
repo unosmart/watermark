e2d6b86fbf4c924f7463f62939ad29ff5d5d5307
Anton <toxa.simply@google.com>
2016-03-05 00:12:48 +0200

    Added opacity changing + redone the _content block

--- source/js/_modules/opacity.js
+++ source/js/_modules/opacity.js
@@ -1,12 +1,42 @@
-(function() {
-      $( "#slider-range-min" ).slider({
-        range: "min",
-        value: 37,
-        min: 1,
-        max: 700,
-        slide: function( event, ui ) {
-          $( "#amount" ).val( "$" + ui.value );
-        }
-      });
-      $( "#amount" ).val( "$" + $( "#slider-range-min" ).slider( "value" ) );
+/**
+ * Module that is resposible for changing opacity of the watermark.
+ * @return {object} This module returns an object with methods inside.
+ */
+;(function() {
+	var opacityModule = {};
+
+	publicMethod();
+	init();
+	attachEvents();
+
+	function init() {
+		/**
+		 * Method slide changes the opacity of watermark while sliding the handle.
+		 * @param  {object} event  Event object.
+		 * @param  {object} ui)    {					var    imgOpacity    Contains determining of position of the slider converted to the opacity units. 
+		 * @param  {property} value: 100		}       Property that sets the initial position of the handle.
+		 */
+		$("#slider").slider({
+				slide: function(event, ui) {
+					var imgOpacity = ui.value * 0.01;
+				
+					$('#watermark').css('opacity', imgOpacity);
+				},
+
+				value: 100
+		});
+	};
+
+	function attachEvents() {
+	
+	};
+
+	function publicMethod() {
+		opacityModule = {
+			
+		}
+	};
+
+	window.moduleName = opacityModule;
 })();
+
