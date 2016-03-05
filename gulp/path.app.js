e578b405750de573abf21b0c8c9947228a2ba743
donOleg <O.Petrovich.S@yandex.ua>
2016-03-05 18:57:43 +0200

    тестовая версия драг & дроп, изменения в блоке позиционирования

--- gulp/path.app.js
+++ gulp/path.app.js
@@ -3,5 +3,7 @@
 module.exports = [
   './source/js/app.js',
   './source/js/_modules/opacity.js',
-  './source/js/_modules/socials.js'
+  './source/js/_modules/socials.js',
+  './source/js/_modules/draggable.js'
+
 ];
