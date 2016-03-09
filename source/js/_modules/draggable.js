1dcc46a7cafc29fd9f1ef25464c16e8648cf1436
donOleg <O.Petrovich.S@yandex.ua>
2016-03-09 20:42:34 +0200

    перемещение по осям

--- source/js/_modules/draggable.js
+++ source/js/_modules/draggable.js
@@ -1,7 +1,13 @@
 $(function() {
 
     $( "#watermark" ).draggable({
-      containment:'parent'
+      containment:'parent',
+
+      drag: function(e) {
+        $('#control_X').val(e.offsetX);
+        $('#control_Y').val(e.offsetY);
+      }
+
     });
 
 });
