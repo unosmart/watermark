814c87bbf944b4141af10fa5b13dfad719528ddf
Konstantin Kudelko <Konstantinkudelko>
2016-03-10 10:56:11 +0300

    Исправление позиционирования 1

--- source/js/_modules/position_watermark.js
+++ source/js/_modules/position_watermark.js
@@ -17,18 +17,36 @@ $(document).ready(function() {
       event.preventDefault();
       image.addClass('top00');
       image.removeClass('content__watermark-block top02 top01 center12 center11 center10 bottom21 bottom20 bottom22');
+      image.css({
+        top: '',
+        left: '',
+        right: '',
+        bottom: ''
+      });;
     });
 
      block01.on('click', function(event) {
       event.preventDefault();
       image.addClass('top01');
       image.removeClass('content__watermark-block center12 center11 center10 bottom21 bottom20 bottom22');
+      image.css({
+        top: '',
+        left: '',
+        right: '',
+        bottom: ''
+      });;
     });
 
      block02.on('click', function(event) {
       event.preventDefault();
       image.addClass('top02');
       image.removeClass('content__watermark-block top01 top00 center12 center11 center10 bottom21 bottom20 bottom22');
+      image.css({
+        top: '',
+        left: '',
+        right: '',
+        bottom: ''
+      });;
     });
 
       // second row
@@ -36,18 +54,36 @@ $(document).ready(function() {
       event.preventDefault();
       image.addClass('center10');
       image.removeClass('content__watermark-block center12 center11 bottom21 bottom22 bottom20');
+      image.css({
+        top: '',
+        left: '',
+        right: '',
+        bottom: ''
+      });;
     });
 
      block11.on('click', function(event) {
       event.preventDefault();
       image.addClass('center11');
       image.removeClass('content__watermark-block bottom21 bottom22 center10 center12 bottom20 bottom21 bottom22');
+      image.css({
+        top: '',
+        left: '',
+        right: '',
+        bottom: ''
+      });;
     });
 
      block12.on('click', function(event) {
       event.preventDefault();
       image.addClass('center12');
-      image.removeClass('content__watermark-block center11 center10 top00 bottom21 bottom22');
+      image.removeClass('content__watermark-block center11 center10 top00 bottom20 bottom21 bottom22');
+      image.css({
+        top: '',
+        left: '',
+        right: '',
+        bottom: ''
+      });;
     });
 
       // third row
@@ -55,17 +91,35 @@ $(document).ready(function() {
       event.preventDefault();
       image.addClass('bottom20');
       image.removeClass('content__watermark-block top01 top00 top02  center12 center11 center10 bottom21 bottom22');
+      image.css({
+        top: '',
+        left: '',
+        right: '',
+        bottom: ''
+      });;
     });
 
      block21.on('click', function(event) {
       event.preventDefault();
       image.addClass('bottom21');
       image.removeClass('content__watermark-block top01 top00 top02 center12 center11 center10 bottom22');
+      image.css({
+        top: '',
+        left: '',
+        right: '',
+        bottom: ''
+      });;
     });
 
      block22.on('click', function(event) {
       event.preventDefault();
       image.addClass('bottom22');
       image.removeClass('content__watermark-block top01 top00 top02 center12 center11 center10 bottom21 bottom20');
+      image.css({
+        top: '',
+        left: '',
+        right: '',
+        bottom: ''
+      });;
     });
 });
