00bda3e62d1a767ecf109a97b8eee1eb1863628c
Konstantin Kudelko <Konstantinkudelko>
2016-03-10 15:54:11 +0300

    Позиционирование в модуле, тестовые инпуты

--- source/js/_modules/position_watermark.js
+++ source/js/_modules/position_watermark.js
@@ -1,125 +1,186 @@
-$(document).ready(function() {
-  var block00 = $('.block00');
-  var block01 = $('.block01');
-  var block02 = $('.block02');
+var myModule = (function (){
 
-  var block10 = $('.block10');
-  var block11 = $('.block11');
-  var block12 = $('.block12');
-
-  var block20 = $('.block20');
-  var block21 = $('.block21');
-  var block22 = $('.block22');
+  var init = function () {
+    _setUpListners();
+  };
   var image = $('#watermark');
 
-    // first row
-    block00.on('click', function(event) {
-      event.preventDefault();
-      image.addClass('top00');
+  var _setUpListners = function () {
+    $('#single').on('click', _singleMode); // открыть singleMode
+    $('.block00').on('click', _block00);
+    $('.block01').on('click', _block01);
+    $('.block02').on('click', _block02);
+
+    $('.block10').on('click', _block10);
+    $('.block11').on('click', _block11);
+    $('.block12').on('click', _block12);
+
+    $('.block20').on('click', _block20);
+    $('.block21').on('click', _block21);
+    $('.block22').on('click', _block22);
+
+  };
+  //Переключение 
+  var _singleMode = function (e) {
+    e.preventDefault();
+    var singleMode = $('#single'),
+        tilingMode = $('#tessel');
+
+
+    singleMode.addClass('setting-pos__toggle-single_active');
+    tilingMode.removeClass('setting-pos__toggle-tessel_active');
+  };
+
+  var _block00 = function (e) {
+    e.preventDefault();
+    image.addClass('top00');
       image.removeClass('content__watermark-block top02 top01 center12 center11 center10 bottom21 bottom20 bottom22');
       image.css({
         top: '',
         left: '',
         right: '',
         bottom: ''
-      });;
-    });
+      });
+    var top = parseInt(image.css('top'));
+    var left = image.css('left');
+    $('#control_X').val(left);
+    $('#control_Y').val(top);
+    console.log(top);
+  }
 
-     block01.on('click', function(event) {
-      event.preventDefault();
-      image.addClass('top01');
-      image.removeClass('content__watermark-block center12 center11 center10 bottom21 bottom20 bottom22');
+  var _block01 = function (e) {
+    e.preventDefault();
+    image.addClass('top01');
+      image.removeClass('content__watermark-block top00 top02 center12 center11 center10 bottom21 bottom20 bottom22');
       image.css({
         top: '',
         left: '',
         right: '',
         bottom: ''
-      });;
-    });
+      });
+    var top = parseInt(image.css('top'));
+    var left = image.css('left');
+    $('#control_Y').val(top);
+    $('#control_X').val(left);
 
-     block02.on('click', function(event) {
-      event.preventDefault();
-      image.addClass('top02');
-      image.removeClass('content__watermark-block top01 top00 center12 center11 center10 bottom21 bottom20 bottom22');
+  }
+
+  var _block02 = function (e) {
+    e.preventDefault();
+    image.addClass('top02');
+      image.removeClass('content__watermark-block top00 top01 center12 center11 center10 bottom21 bottom20 bottom22');
       image.css({
         top: '',
         left: '',
         right: '',
         bottom: ''
-      });;
-    });
+      });
+    var top = parseInt(image.css('top'));
+    var right = image.css('left');
+    $('#control_Y').val(top);
+    $('#control_X').val(right);
+    console.log($('#control_y'));
+  }
 
-      // second row
-      block10.on('click', function(event) {
-      event.preventDefault();
-      image.addClass('center10');
-      image.removeClass('content__watermark-block center12 center11 bottom21 bottom22 bottom20');
+  var _block10 = function (e) {
+    e.preventDefault();
+    image.addClass('center10');
+    image.removeClass('content__watermark-block top00 top01 top02 center11 center12 bottom21 bottom20 bottom22');
       image.css({
         top: '',
         left: '',
         right: '',
         bottom: ''
-      });;
-    });
+      });
+    var top = parseInt(image.css('top'));
+    var left = image.css('left');
+    $('#control_Y').val(top);
+    $('#control_X').val(left);
+  }
 
-     block11.on('click', function(event) {
-      event.preventDefault();
-      image.addClass('center11');
-      image.removeClass('content__watermark-block bottom21 bottom22 center10 center12 bottom20 bottom21 bottom22');
+  var _block11 = function (e) {
+    e.preventDefault();
+    image.addClass('center11');
+      image.removeClass('content__watermark-block top02 top01 top00 center12 center10 bottom21 bottom20 bottom22');
       image.css({
         top: '',
         left: '',
         right: '',
         bottom: ''
-      });;
-    });
+      });
+    var top = parseInt(image.css('top'));
+    var left = image.css('left');
+    $('#control_Y').val(top);
+    $('#control_X').val(left);
+  }
 
-     block12.on('click', function(event) {
-      event.preventDefault();
-      image.addClass('center12');
-      image.removeClass('content__watermark-block center11 center10 top00 bottom20 bottom21 bottom22');
+  var _block12 = function (e) {
+    e.preventDefault();
+    image.addClass('center12');
+      image.removeClass('content__watermark-block top02 top01 top00 center11 center10 bottom21 bottom20 bottom22');
       image.css({
         top: '',
         left: '',
         right: '',
         bottom: ''
-      });;
-    });
+      });
+    var top = parseInt(image.css('top'));
+    var right = image.css('left');
+    $('#control_Y').val(top);
+    $('#control_X').val(right);
+  }
 
-      // third row
-      block20.on('click', function(event) {
-      event.preventDefault();
-      image.addClass('bottom20');
-      image.removeClass('content__watermark-block top01 top00 top02  center12 center11 center10 bottom21 bottom22');
+  var _block20 = function (e) {
+    e.preventDefault();
+    image.addClass('bottom20');
+      image.removeClass('content__watermark-block top02 top01 center12 center11 center10 bottom21 bottom22');
       image.css({
         top: '',
         left: '',
         right: '',
         bottom: ''
-      });;
-    });
+      });
+    var top = parseInt(image.css('bottom'));
+    var left = image.css('left');
+    $('#control_Y').val(top);
+    $('#control_X').val(left);
+  }
 
-     block21.on('click', function(event) {
-      event.preventDefault();
-      image.addClass('bottom21');
-      image.removeClass('content__watermark-block top01 top00 top02 center12 center11 center10 bottom22');
+  var _block21 = function (e) {
+    e.preventDefault();
+    image.addClass('bottom21');
+      image.removeClass('content__watermark-block top02 top01 center12 center11 center10 bottom20 bottom22');
       image.css({
         top: '',
         left: '',
         right: '',
         bottom: ''
-      });;
-    });
+      });
+    var top = parseInt(image.css('bottom'));
+    var left = image.css('left');
+    $('#control_Y').val(top);
+    $('#control_X').val(left);
+  }
 
-     block22.on('click', function(event) {
-      event.preventDefault();
-      image.addClass('bottom22');
-      image.removeClass('content__watermark-block top01 top00 top02 center12 center11 center10 bottom21 bottom20');
+  var _block22 = function (e) {
+    e.preventDefault();
+    image.addClass('bottom22');
+      image.removeClass('content__watermark-block top02 top01 center12 center11 center10 bottom21 bottom20');
       image.css({
         top: '',
         left: '',
         right: '',
         bottom: ''
-      });;
-    });
-});
+      });
+    var top = parseInt(image.css('bottom'));
+    var right = image.css('left');
+    $('#control_Y').val(top);
+    $('#control_X').val(right);
+  }
+  
+  return {
+    init: init
+  };
+
+})();
+myModule.init();
